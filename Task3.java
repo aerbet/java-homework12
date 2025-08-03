16a1d4d64179032d6986dbe1bb2f8cd2901a1f23
Erbol Zhaparov <ae.270718@gmail.com>
2025-08-03 15:59:28 +0600

    finished task 3 & 4

--- Task3.java
+++ Task3.java
@@ -1,8 +1,25 @@
+import java.util.Arrays;
 
 public class Task3 {
     
     public static void main(String[] args) {
-        
-       
+
+        java.util.Random rnd = new java.util.Random();
+
+        int min = 163;
+        int max = 190;
+
+        int[] peopleArr = new int[12];
+
+        for (int i = 0; i < peopleArr.length; i++) {
+            int value = rnd.nextInt(max - min + 1) + min;
+            peopleArr[i] = value;
+        }
+
+        String tmpl = "%-23s %s\n";
+
+        System.out.printf(tmpl, "Исходный массив: ", Arrays.toString(peopleArr));
+        java.util.Arrays.sort(peopleArr);
+        System.out.printf(tmpl, "Отсортрованный массив: ", Arrays.toString(peopleArr));
     }
 }
--- Task4.java
+++ Task4.java
@@ -1,8 +1,34 @@
-import java.util.Scanner;
+
+import java.util.Arrays;
+
 
 public class Task4 {
     
     public static void main(String[] args) {
+        java.util.Random rnd = new java.util.Random();
+
+        int value = rnd.nextInt(15 - 5 + 1) + 5;
+
+        int[] numsArr = new int[value];
+
+        numsArr[0] = 1;
+        numsArr[1] = 1;
+
+        String tmpl = "%-15s %s\n";
+
+        for (int i = 2; i < numsArr.length; i++) {
+            numsArr[i] = numsArr[i - 1] + numsArr[i - 2];
+        }
+
+        System.out.printf(tmpl, "Исходный массив: ", Arrays.toString(numsArr));
         
-    }
+        for (int i = 0; i < numsArr.length / 2; i++) {
+            int rev = numsArr[i];
+
+            numsArr[i] = numsArr[numsArr.length - i - 1];
+            numsArr[numsArr.length - i - 1] = rev;
+        }
+
+        System.out.printf(tmpl, "Обратный массив: ", Arrays.toString(numsArr));
+     }
 }
