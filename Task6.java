1cc0c67fafb5a6f8531002627368e13d7ed5c932
Erbol Zhaparov <ae.270718@gmail.com>
2025-08-04 17:42:48 +0600

    finished task6

--- Task6.java
+++ Task6.java
@@ -1,7 +1,48 @@
+import java.util.Arrays;
 
 public class Task6 {
     
     public static void main(String[] args) {
-       
+       java.util.Random rnd = new java.util.Random();
+
+        int arrLength = rnd.nextInt(10 - 3 + 1) + 3;
+
+        int[] numsArr = new int[arrLength];
+        int[] ngtArr = new int[arrLength];
+        int[] posArr = new int[arrLength];
+
+        int[] evenNums = new int[arrLength];
+        int[] oddNums = new int[arrLength];
+
+        for (int i = 0; i < numsArr.length; i++) {
+            int value = rnd.nextInt(10 - (-10) + 1) - 10;
+
+            numsArr[i] += value;
+
+            if (value < 0) {
+                ngtArr[i] += value;
+            } else {
+                posArr[i] += value;
+            }
+
+            if (value % 2 == 0) {
+                evenNums[i] += value;
+            } else {
+                oddNums[i] += value;
+            }
+        }
+
+        int[] newArr = Arrays.stream(evenNums).filter(x -> x != 0).toArray();
+        int[] newArr2 = Arrays.stream(oddNums).filter(x -> x != 0).toArray();
+
+        String tmpl = "Количество четных чисел: %d\nКоличество нечетных чисел: %d";
+
+        System.out.println(Arrays.toString(numsArr));
+        System.out.println(Arrays.toString(Arrays.stream(ngtArr).filter(x -> x != 0).toArray()));
+        System.out.println(Arrays.toString(Arrays.stream(posArr).filter(x -> x != 0).toArray()));
+        System.out.println(Arrays.toString(newArr));
+        System.out.println(Arrays.toString(newArr2));
+
+        System.out.printf(tmpl, newArr.length, newArr2.length);
     }
 }
